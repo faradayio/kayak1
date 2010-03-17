ee21e098cc925a9c808aef7112879413cbdc2ef7
Andy Rossmeissl <andy@rossmeissl.net>
2010-03-16 21:02:27 -0400

    Stubs

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,19 @@
 class SearchesController < ApplicationController
   def new
+    @search = Search.new
+  end
+  
+  def create
+    @search = Search.new params[:search]
+    if @search.save
+      redirect_to @search
+    else
+      flash[:notice] = 'There was a problem with your search'
+      render :action => :new
+    end
+  end
+  
+  def show
+    @search = Search.find params[:id]
   end
 end
---
+++ app/models/search.rb
@@ -0,0 +1,5 @@
+class Search < ActiveRecord::Base
+  def itineraries
+    # pull from kayak
+  end
+end
