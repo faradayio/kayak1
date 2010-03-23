b450ab9e78c6d13380b9a8e5a6084635033c9cce
Andy Rossmeissl <andy@rossmeissl.net>
2010-03-23 08:09:58 -0400

    Send each segment as emplanements_per_trip => 1, trips => 1

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -11,13 +11,15 @@ class Segment < Weary::Base
   
   post :calculate do |resource|
     resource.url = 'http://carbon.brighterplanet.com/flights.json'
-    resource.requires = [:'flight[origin_airport]', :'flight[destination_airport]', :'flight[airline]']
+    resource.requires = [:'flight[origin_airport]', :'flight[destination_airport]', :'flight[airline]', :'flight[emplanements_per_trip]', :'flight[trips]']
   end
   
   def emission
     calculate(:'flight[origin_airport]' => { :iata_code => origin_airport },
               :'flight[destination_airport]' => { :iata_code => destination_airport },
-              :'flight[airline]' => { :iata_code => airline }
+              :'flight[airline]' => { :iata_code => airline },
+              :'flight[emplanements_per_trip]' => 1,
+              :'flight[trips]' => 1
               ).perform.parse.symbolize_keys[:emission]
   end
   
