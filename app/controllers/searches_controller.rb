7c25be34fce9eacb59288aac747c952853735126
Andy Rossmeissl <andy@rossmeissl.net>
2010-03-24 07:51:50 -0400

    Sleep for debug

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,6 +5,7 @@ class SearchesController < ApplicationController
   
   def create
     @search = Search.new(params.symbolize_keys.slice(:origin_airport, :destination_airport, :date)).tap(&:perform!)
+    sleep 3
     @itineraries = Nokogiri::XML(@search.itineraries.body).css('searchresult trips trip').to_a.map { |node| Itinerary.from_node node }
     render :action => :show
   end
