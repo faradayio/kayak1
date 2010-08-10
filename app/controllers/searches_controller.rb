33cefc59ec8c292cc09caba5f2e6088ae86afa19
Andy Rossmeissl <andy@rossmeissl.net>
2010-08-10 10:40:40 -0500

    Show screencast on rate-limit error

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -17,6 +17,10 @@ class SearchesController < ApplicationController
       xml = @search.itineraries.body
     end
     @itineraries = Nokogiri::XML(xml).css('searchresult trips trip').to_a.map { |node| Itinerary.from_node node }[0..(RESULTS_TO_FETCH - 1)]
-    render :action => :show
+    begin
+      render :action => :show
+    rescue ActionView::TemplateError
+      render :status => 503, :file => Rails.root + 'public/503.html'
+    end
   end
 end
